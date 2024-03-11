eb0c873009a538c499a8eaaa591fc278601bc011
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2024-03-11 16:12:08 +1100

    add reliable animation using motion-one

--- src/interface/components/Switch.svelte
+++ src/interface/components/Switch.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { settingsState } from '../state/SettingsState.ts';
-  import { spring } from "svelte/motion";
-  import { MotionDiv } from "svelte-motion";
+  import { animate, spring } from 'motion';
   import './Switch.css'
   import { onMount } from "svelte"
   import { delay } from "../../seqta/utils/delay"
@@ -19,11 +18,31 @@
     stiffness: 700,
     damping: 30,
   }
+
+  let handle;
+
+  const animation = (enabled) => {
+    if (handle) {
+      animate(
+        handle,
+        {
+          x: enabled ? 24 : 0,
+        },
+        {
+          easing: spring({ stiffness: 500, damping: 30 })
+        }
+      )
+    }
+  }
+
+  $: {
+    animation($settingsState[setting])
+  }
 </script>
 
 <div
   id={setting}
-  class="flex w-14 p-1 cursor-pointer rounded-full dark:bg-[#38373D] bg-[#DDDDDD] switch"
+  class="flex w-14 p-1 cursor-pointer transition rounded-full dark:bg-[#38373D] bg-[#DDDDDD] switch"
   data-ison={$settingsState[setting]}
   on:click={toggleSwitch}
   on:keydown={(e) => e.key === "Enter" && toggleSwitch()}
@@ -31,9 +50,8 @@
   aria-checked={$settingsState[setting]}
   tabindex="0"
 >
-  <MotionDiv
-    animate={{ x: $settingsState[setting] ? 24 : 0 }}
-    transition={springParams}
+  <div
+    bind:this={handle}
     class="w-6 h-6 bg-white dark:bg-[#FEFEFE] rounded-full drop-shadow-md"
   />
 </div>
